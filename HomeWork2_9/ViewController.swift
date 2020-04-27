e555c0f3cfdee3e913c306dfb61b4ee681b279b2
moremist <rbest15@rambler.ru>
2020-04-27 11:00:01 +0300

    Initial Commit

---
+++ HomeWork2_9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork2_9
+//
+//  Created by Roman Kuzmich on 27.04.2020.
+//  Copyright © 2020 Roman Kuzmich Home Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
