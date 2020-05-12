fa4bb9a3e8650d7a486507b4105873e829eaf02e
moremist <rbest15@rambler.ru>
2020-05-12 15:12:04 +0300

    Initial view screen added

--- HomeWork2_9/AppDelegate.swift
+++ HomeWork2_9/AppDelegate.swift
@@ -1,37 +1,57 @@
-//
-//  AppDelegate.swift
-//  HomeWork2_9
-//
-//  Created by Roman Kuzmich on 27.04.2020.
-//  Copyright © 2020 Roman Kuzmich Home Dev. All rights reserved.
-//
-
 import UIKit
+import FBSDKLoginKit
+import VK_ios_sdk
+import GoogleSignIn
+import TwitterKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    func application( _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? ) -> Bool {
+        ApplicationDelegate.shared.application( application, didFinishLaunchingWithOptions: launchOptions )
+        VKSdk.initialize(withAppId: "7427495")?.uiDelegate = self
+        GIDSignIn.sharedInstance()?.clientID = "1084893895308-o8ksmcrtfrimc2k9h1ussvpj43j60sir.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance()?.delegate = self
+        
+        TWTRTwitter.sharedInstance().start(withConsumerKey: "rdfghdfg", consumerSecret: "dgfhdfgh")
+        return true
+        
+    }
+    
+    func application( _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool {
+        
+        ApplicationDelegate.shared.application( app, open: url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String, annotation: options[UIApplication.OpenURLOptionsKey.annotation] )
+        
+        VKSdk.processOpen(url, fromApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String)
+        
+        GIDSignIn.sharedInstance()?.handle(url)
+        
+        TWTRTwitter.sharedInstance().application(app, open: url, options: options[UIApplication.OpenURLOptionsKey.sourceApplication] as! [AnyHashable : Any])
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
+extension AppDelegate : VKSdkUIDelegate {
+    func vkSdkShouldPresent(_ controller: UIViewController!) {
+        let vc = UIApplication.shared.keyWindow?.rootViewController
+        if vc?.presentedViewController != nil {
+            vc?.dismiss(animated: true, completion: {
+                vc?.present(controller, animated: true)
+            })
+        } else {
+            vc?.present(controller,animated: true)
+        }
+    }
+    
+    func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
+        
+    }
+}
+
+extension AppDelegate: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        print("ДОСТУП К ГУГЛ ПОЛУЧЕН", user.authentication.accessToken)
+    }
+}
--- HomeWork2_9/ViewController.swift
+++ HomeWork2_9/ViewController.swift
@@ -1,20 +1,116 @@
-//
-//  ViewController.swift
-//  HomeWork2_9
-//
-//  Created by Roman Kuzmich on 27.04.2020.
-//  Copyright © 2020 Roman Kuzmich Home Dev. All rights reserved.
-//
-
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FBSDKShareKit
+import VK_ios_sdk
+import TwitterKit
+import GoogleSignIn
+import Social
+import AppAuth
 
-class ViewController: UIViewController {
 
+@IBDesignable class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    let fbLogin = FBLoginButton()
+    let vkLogin = UIButton()
+    let twittLogin = TWTRLogInButton()
+    let googleLogin = GIDSignInButton()
+    let stackView = UIStackView()
+    let stackViewSpacing = CGFloat(5)
+    let buttonsCount = 4
+    
+    let viewImagePick = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureStackViewWithLogins()
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        VKSdk.initialize(withAppId: "7427495")?.register(self)
     }
-
-
+    @IBAction func pickImage(_ sender: Any) {
+        let ipc = UIImagePickerController()
+        ipc.delegate = self
+        ipc.allowsEditing = true
+        present(ipc, animated: true)
+    }
+    
+    @IBAction func shareButton(_ sender: Any) {
+        guard (imageView.image) != nil else {
+            return
+        }
+        if imageView.image != nil {
+            let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: nil)
+            vc.title = "Share this image!"
+            present(vc, animated: true)
+        }
+    }
+    
+    fileprivate func configureStackViewWithLogins() {
+        imageView.contentMode = .scaleToFill
+        
+        fbLogin.delegate = self
+        fbLogin.permissions = ["email"]
+        
+        vkLogin.setTitle("Sign in with VK", for: .normal)
+        vkLogin.addTarget(self, action: #selector(loginToVK), for: .touchUpInside)
+        vkLogin.backgroundColor = .blue
+        vkLogin.layer.cornerRadius = 3
+        
+        stackView.addArrangedSubview(fbLogin)
+        stackView.addArrangedSubview(vkLogin)
+        stackView.addArrangedSubview(twittLogin)
+        stackView.addArrangedSubview(googleLogin)
+        
+        
+        let  stackViewHeight = CGFloat(200)
+        stackView.bounds = CGRect(x: 0, y: 0, width: 300, height: stackViewHeight)
+        stackView.spacing = stackViewSpacing
+        stackView.center.x = view.center.x
+        stackView.center.y = view.center.y + view.center.y / 2
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        
+        view.addSubview(stackView)
+    }
+    
+    @objc func loginToVK(){
+        VKSdk.authorize(["friends"])
+    }
+    
 }
 
+extension ViewController : LoginButtonDelegate {
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        print("ДОСТУП К FB ПОЛУЧЕН", result?.token)
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        print("FB LOG OUT")
+    }
+    
+    
+}
+
+extension ViewController : VKSdkDelegate {
+    func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
+        print("ДОСТУП К ВК ПОЛУЧЕН", result.token)
+    }
+    
+    func vkSdkUserAuthorizationFailed() {
+        print("VK AUTH FAILED")
+    }
+    
+    override func vks_viewControllerWillDismiss() {
+        print("dissmised VK")
+    }
+}
+
+extension ViewController : UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else{ return }
+        imageView.image = image
+        dismiss(animated: true, completion: nil)
+    }
+}
